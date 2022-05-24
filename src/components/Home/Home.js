dc8cdbab42365397f74a6542b765240c6ca1d4ac
EliTejeda <tejeda.eliza@gmail.com>
2022-05-24 17:36:58 -0500

    CSS en vista Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,28 +5,39 @@ import { IoExitSharp, IoAddCircleSharp } from 'react-icons/io5';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebaseConfig';
 // import CreateNotes from '../Notes/CreateNotes';
-import NoteOne from '../Notes/NoteOne';
+// import NoteOne from '../Notes/NoteOne';
+import logoTitle from '../../assets/img/logoTitle.png';
+import './Home.css';
 
 export default function Home() {
-  const noteOne = { title: 'Pendientes', date: '20/05/2022', text: 'Que sirva esta vaina' };
-  const noteTwo = { title: 'Chores', date: '19/05/2022', text: 'Sacar a pasear al perro' };
+  /* const noteOne = { title: 'Pendientes', date: '20/05/2022', text: 'Que sirva esta vaina' };
+  const noteTwo = { title: 'Chores', date: '19/05/2022', text: 'Sacar a pasear al perro' }; */
   const navigate = useNavigate();
+
   const handleLogOut = () => {
     signOut(auth)
       .then(() => navigate('/'))
       .catch((error) => console.log(error));
   };
   const handleAddNote = () => {
-    navigate('/CreateNotes');
+    navigate('/NoteOne');
   };
 
   return (
-    <>
-      <div className="notes-list">Aqui van las notas</div>
-      <NoteOne props={noteOne} />
-      <NoteOne props={noteTwo} />
-      <IoExitSharp type="submit" onClick={handleLogOut} size="4.3em" />
-      <IoAddCircleSharp type="submit" onClick={handleAddNote} size="4.3em" />
-    </>
+    <section className="homeContainer">
+
+      <nav className="menuTop">
+        <img src={logoTitle} alt="logo_anota" width="100px" />
+        <IoExitSharp type="submit" onClick={handleLogOut} size="4.3em" />
+      </nav>
+      <section className="notesContainer" />
+      <footer className="menuBottom">
+        <IoAddCircleSharp className="addNote" type="submit" onClick={handleAddNote} size="4.3em" />
+      </footer>
+    </section>
+
+  /* <NoteOne props={noteOne} />
+      <NoteOne props={noteTwo} /> */
+
   );
 }
