6dd30b469867d6f73269e298b000c3bf69ed98ae
EliTejeda <tejeda.eliza@gmail.com>
2022-06-10 10:06:09 -0500

    Borrar notas, icono de borrado

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,22 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { IoExitSharp, IoAddCircleSharp } from 'react-icons/io5';
+import {
+  IoExitSharp,
+  IoAddCircleSharp,
+  IoTrashBinSharp,
+} from 'react-icons/io5';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 import { signOut } from 'firebase/auth';
 import {
-  collection, query, orderBy, getDocs,
+  collection, query, orderBy, getDocs, doc, deleteDoc,
 } from 'firebase/firestore';
 import { auth, db } from '../../firebase/firebaseConfig';
 import logoTitle from '../../assets/img/logoTitle.png';
 import './Home.css';
+
+const MySwal = withReactContent(Swal);
 
 export default function Home() {
   const navigate = useNavigate();
@@ -36,6 +44,7 @@ export default function Home() {
     const post = await getDocs(q);
     console.log(post);
 
+    // eslint-disable-next-line no-shadow
     post.forEach((doc) => {
       if (doc.data().userID === uid) {
         arrayNotesList.push({ ...doc.data(), id: doc.id });
@@ -43,6 +52,32 @@ export default function Home() {
     });
     setNotesList(arrayNotesList);
   };
+
+  // BORRA
+  const deleteItem = async (id) => {
+    const noteReferenceId = doc(db, 'notes', id);
+    await deleteDoc(noteReferenceId);
+    getNoteList();
+  };
+
+  // SWEET ALERT...
+  const handleDeleteItem = (id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteItem(id);
+        MySwal.fire('Deleted!', 'Your file has been deleted.', 'success');
+      }
+    });
+  };
+
   // useEffect sirve para que renderice por primera vez
   useEffect(() => {
     getNoteList();
@@ -61,9 +96,14 @@ export default function Home() {
           <div className="boxNotes" key={notes.id}>
             <div className="notesTitle">
               {notes.title}
-              <p className="notesText">
-                {notes.note}
-              </p>
+              <p className="notesText">{notes.note}</p>
+              <IoTrashBinSharp
+                className="deleteItem"
+                type="button"
+                size="2.3em"
+                onClick={() => { handleDeleteItem(notes.id); }}
+
+              />
             </div>
           </div>
         ))}
